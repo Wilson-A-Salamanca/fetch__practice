777eb5a8b0be92ddd9f3d8391f2b5641d29de55b
Wilson-A-Salamanca <wilsonsalamanca346@gmail.com>
2022-04-04 11:41:16 -0500

    add html and js

---
+++ app.js
@@ -0,0 +1,18 @@
+const cards = 'https://jsonplaceholder.typicode.com/posts'
+const collection = document.getElementById("cards")
+
+fetch(cards)
+.then(data => data.json())
+.then(json => json.forEach(element => {
+    const card = document.createElement("section")
+    const title = document.createElement("h2")
+    const content = document.createElement("p")
+
+    title.textContent = element.title
+    content.textContent = element.body
+
+    card.appendChild(title)
+    card.appendChild(content)
+
+    collection.appendChild(card)
+}))
